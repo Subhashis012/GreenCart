ed9c1fadcea76d1aa4edcd4b9c4990c3d2fcf8fd
Subhashis <subhashisdhara123@gmail.com>
2025-04-12 13:01:56 +0530

    fix: correct request parameter in stripeWebhook function

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -119,7 +119,7 @@ export const stripeWebhook = async (req, res) => {
 
   try {
     event = stripeInstance.webhooks.constructEvent(
-      request.body,
+      req.body,
       sig,
       process.env.STRIPE_WEBHOOK_SECRET
     );
